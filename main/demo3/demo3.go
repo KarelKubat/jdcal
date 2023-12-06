1151365bd5eba67b0019a76b6764f75b363a60dd
Karel Kubat <karel@kubat.nl>
2023-12-07 00:35:02 +0100

    comment fmt fix

--- main/demo3/demo3.go
+++ main/demo3/demo3.go
@@ -13,32 +13,32 @@ func main() {
 
 	// Output (actual string representations may differ):
 	//   Belgium (Southern Netherlands)
-	// 	Started using the Julian    calendar   on   Gregorian -0500/02/28
-	// 	Switched to   the Gregorian calendar   on   Julian 1582/12/20
+	// 		Started using the Julian    calendar   on   Gregorian -0500/02/28
+	// 		Switched to   the Gregorian calendar   on   Julian 1582/12/20
 	//   Netherlands (Brabant)
-	// 	Started using the Julian    calendar   on   Gregorian -0500/02/28
-	// 	Switched to   the Gregorian calendar   on   Julian 1582/12/14
+	// 		Started using the Julian    calendar   on   Gregorian -0500/02/28
+	// 		Switched to   the Gregorian calendar   on   Julian 1582/12/14
 	//   Netherlands (Drenthe)
-	// 	Started using the Julian    calendar   on   Gregorian -0500/02/28
-	// 	Switched to   the Gregorian calendar   on   Julian 1701/04/30
+	// 		Started using the Julian    calendar   on   Gregorian -0500/02/28
+	// 		Switched to   the Gregorian calendar   on   Julian 1701/04/30
 	//   Netherlands (Frisia)
-	// 	Started using the Julian    calendar   on   Gregorian -0500/02/28
-	// 	Switched to   the Gregorian calendar   on   Julian 1701/12/31
+	// 		Started using the Julian    calendar   on   Gregorian -0500/02/28
+	// 		Switched to   the Gregorian calendar   on   Julian 1701/12/31
 	//   Netherlands (Gelderland)
-	// 	Started using the Julian    calendar   on   Gregorian -0500/02/28
-	// 	Switched to   the Gregorian calendar   on   Julian 1700/06/12
+	// 		Started using the Julian    calendar   on   Gregorian -0500/02/28
+	// 		Switched to   the Gregorian calendar   on   Julian 1700/06/12
 	//   Netherlands (Groningen City)
-	// 	Started using the Julian    calendar   on   Gregorian -0500/02/28
-	// 	Switched to   the Gregorian calendar   on   Julian 1583/01/01
-	// 	Switched to   the Julian    calendar   on   Gregorian 1594/11/10
-	// 	Switched to   the Gregorian calendar   on   Julian 1700/12/31
+	// 		Started using the Julian    calendar   on   Gregorian -0500/02/28
+	// 		Switched to   the Gregorian calendar   on   Julian 1583/01/01
+	// 		Switched to   the Julian    calendar   on   Gregorian 1594/11/10
+	// 		Switched to   the Gregorian calendar   on   Julian 1700/12/31
 	//   Netherlands (Holland)
-	// 	Started using the Julian    calendar   on   Gregorian -0500/02/28
-	// 	Switched to   the Gregorian calendar   on   Julian 1583/01/01
+	// 		Started using the Julian    calendar   on   Gregorian -0500/02/28
+	// 		Switched to   the Gregorian calendar   on   Julian 1583/01/01
 	//   Netherlands (Utrecht, Overijssel)
-	// 	Started using the Julian    calendar   on   Gregorian -0500/02/28
-	// 	Switched to   the Gregorian calendar   on   Julian 1700/11/30
+	// 		Started using the Julian    calendar   on   Gregorian -0500/02/28
+	// 		Switched to   the Gregorian calendar   on   Julian 1700/11/30
 	//   Netherlands (Zeeland, States General)
-	// 	Started using the Julian    calendar   on   Gregorian -0500/02/28
-	// 	Switched to   the Gregorian calendar   on   Julian 1582/12/14
+	// 		Started using the Julian    calendar   on   Gregorian -0500/02/28
+	// 		Switched to   the Gregorian calendar   on   Julian 1582/12/14
 }
