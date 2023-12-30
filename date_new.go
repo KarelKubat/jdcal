6d7ef6e299355af9735f5b91ed4531f54cd8ff51
Karel Kubat <karel@kubat.nl>
2023-12-30 09:51:32 +0100

    NewFromString refactoring

--- date_new.go
+++ date_new.go
@@ -6,19 +6,30 @@ import "time"
 New is a helper function to construct a Date from a year, month, day and calendar type.
 The two snippets are equivalent:
 
-  d, err := jdcal.New(1962, time.August, 19, jdcal.Gregorian)
-  if err != nil {...}
+	d, err := jdcal.New(1962, time.August, 19, jdcal.Gregorian)
+	if err != nil {...}
 
-  d := Date{Year: 1962, Month: time.August, Day: 19, Type: jdcal.Gregorian}
-  err := d.Valid()
-  if err {...}
+	d := Date{Year: 1962, Month: time.August, Day: 19, Type: jdcal.Gregorian}
+	err := d.Valid()
+	if err {...}
 */
-func New(year int, month time.Month, day int, tp Type) (Date, error) {
-	dt := Date{
+func New(year int, month time.Month, day int, tp Type) (dt Date, err error) {
+	dt = Date{
 		Year:  year,
 		Month: month,
 		Day:   day,
 		Type:  tp,
 	}
 	return dt, dt.Valid()
+}
+
+/*
+NewFromString is a helper function to convert a string in the format "YYYY/MM/DD" into a date of a given Type (Julian or Gregorian). It chaines StringToYMD() and New().
+*/
+func NewFromString(arg string, tp Type) (dt Date, err error) {
+	y, m, d, err := StringToYMD(arg)
+	if err != nil {
+		return dt, err
+	}
+	return New(y, m, d, tp)
 }
--- main/jdcal/cmd/convert/convert.go
+++ main/jdcal/cmd/convert/convert.go
@@ -3,9 +3,6 @@ package convert
 import (
 	"fmt"
 	"log"
-	"strconv"
-	"strings"
-	"time"
 
 	"github.com/KarelKubat/jdcal"
 	"github.com/spf13/cobra"
@@ -41,37 +38,20 @@ func runConvert(cmd *cobra.Command, args []string) {
 	check(err)
 	for _, a := range args {
 		if gotG {
-			dt := argToDate(a, jdcal.Gregorian)
+			dt, err := jdcal.NewFromString(a, jdcal.Gregorian)
+			check(err)
 			ot, err := dt.Convert()
 			check(err)
 			fmt.Println(dt, "is", ot)
 		}
 		if gotJ {
-			dt := argToDate(a, jdcal.Julian)
+			dt, err := jdcal.NewFromString(a, jdcal.Julian)
+			check(err)
 			ot, err := dt.Convert()
 			check(err)
 			fmt.Println(dt, "is", ot)
 		}
 	}
-}
-
-func argToDate(arg string, tp jdcal.Type) jdcal.Date {
-	parts := strings.Split(arg, "/")
-	if len(parts) != 3 {
-		check(fmt.Errorf("malformed date %q, want YYYY/MM/DD", arg))
-	}
-	dt, err := jdcal.New(atoi(arg, parts[0]), time.Month(atoi(arg, parts[1])),
-		atoi(arg, parts[2]), tp)
-	check(err)
-	return dt
-}
-
-func atoi(fullArg, arg string) int {
-	r, err := strconv.Atoi(arg)
-	if err != nil {
-		check(fmt.Errorf("part %q of %q is not a number: %v", arg, fullArg, err))
-	}
-	return r
 }
 
 func check(err error) {
