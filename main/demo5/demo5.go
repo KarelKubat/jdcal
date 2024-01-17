0d8169f6dccbdc3a8c4a36ffdb0e5b941bfe5589
Karel Kubat <karel@kubat.nl>
2024-01-17 17:05:36 +0100

    docfix

--- main/demo5/demo5.go
+++ main/demo5/demo5.go
@@ -12,11 +12,7 @@ func main() {
 	for _, yr := range []jdcal.Year{1369, 1370, 1371} {
 		cyr := jdcal.CalendarYear{Year: yr, Type: jdcal.Julian}
 
-		for _, h := range []jdcal.Holiday{
-			jdcal.Easter,
-			jdcal.Ascension,
-			jdcal.Pentecost,
-		} {
+		for h := jdcal.GoodFriday; h <= jdcal.Pentecost; h++ {
 			dt, err := cyr.HolidayDate(h)
 			check(err)
 			wd, err := dt.Weekday()
@@ -27,16 +23,19 @@ func main() {
 	}
 
 	// Output:
+	// Good Friday in Julian 1369 falls on Friday Julian 1369/03/23
 	// Easter in Julian 1369 falls on Sunday Julian 1369/03/25
-	// Ascension day in Julian 1369 falls on Thursday Julian 1369/05/03
+	// Ascension Day in Julian 1369 falls on Thursday Julian 1369/05/03
 	// Pentecost in Julian 1369 falls on Sunday Julian 1369/05/13
 
+	// Good Friday in Julian 1370 falls on Friday Julian 1370/04/12
 	// Easter in Julian 1370 falls on Sunday Julian 1370/04/14
-	// Ascension day in Julian 1370 falls on Thursday Julian 1370/05/23
+	// Ascension Day in Julian 1370 falls on Thursday Julian 1370/05/23
 	// Pentecost in Julian 1370 falls on Sunday Julian 1370/06/02
 
+	// Good Friday in Julian 1371 falls on Friday Julian 1371/04/04
 	// Easter in Julian 1371 falls on Sunday Julian 1371/04/06
-	// Ascension day in Julian 1371 falls on Thursday Julian 1371/05/15
+	// Ascension Day in Julian 1371 falls on Thursday Julian 1371/05/15
 	// Pentecost in Julian 1371 falls on Sunday Julian 1371/05/25
 }
 
