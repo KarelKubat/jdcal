9b41a1694413145351b686295294bfc481410da5
Karel Kubat <karel@kubat.nl>
2024-01-19 19:03:20 +0100

    support Ordinal type

--- ordinal.go
+++ ordinal.go
@@ -6,22 +6,16 @@ import (
 )
 
 /*
-OrdinalToDate returns the jdcal.Date for a given ordinal day number. This is the reverse of
+Date returns the jdcal.Date for a given ordinal day number. This is the reverse of
 Date.Ordinal.
 */
-func OrdinalToDate(ordinal int, tp Type) (Date, error) {
-	var yr Year
-	for yr = StartProgressionYear; yr <= EndProgressionYear; yr++ {
-		if yr == EndProgressionYear {
-			break
-		}
-		nextProgression := YearProgression[yr+1][tp]
-		if nextProgression > ordinal {
-			break
-		}
-	}
+func (o Ordinal) Date(tp Type) (Date, error) {
+	// Find the year where the ordinal equals the requested one, or where it's (just) below.
+	yr := o.Year(tp)
+	// fmt.Println("OrdinalToDate, ordinal:", o, "for type", tp, "gives yr:", yr)
 
-	var progression map[time.Month][]int
+	// Ordinal days for that year are either by nonleap or by leap.
+	var progression MonthProgression
 	cyr := CalendarYear{Year: yr, Type: tp}
 	if cyr.IsLeap() {
 		progression = LeapMonthProgression
@@ -32,29 +26,61 @@ func OrdinalToDate(ordinal int, tp Type) (Date, error) {
 	// Start looking at the base number of the overall years progression.
 	ord := YearProgression[yr][tp]
 
+	// First/last edge cases
+	missing := o - ord
+	if missing == 0 {
+		return New(yr, time.January, 1, tp)
+	}
+	var max Ordinal
+	if cyr.IsLeap() {
+		max = 365
+	} else {
+		max = 364
+	}
+	if missing == max {
+		return New(yr, time.December, 31, tp)
+	}
+
 	// Find the month that reduces ordinal minus ord the best.
 	var m time.Month
 	for m = time.January; m <= time.December; m++ {
-		max := lastInt(progression[m])
-		if ordinal-ord-max <= 0 {
+		max := lastOrd(progression[m])
+		if missing-max <= 0 {
 			break
 		}
 	}
+	// fmt.Println("OrdinalToDate for", o, ": year start", ord, "missing days:", missing, "best month:", m)
 
 	// Find the day to fill the gap.
-	for nr, d := range progression[m] {
-		if ord+d >= ordinal {
-			return New(yr, m, nr, tp)
+	for day, count := range progression[m] {
+		if day == 0 {
+			continue // Skip fillers
+		}
+		// fmt.Println("OrdinalToDate: day", day, "count", count, "missing", missing)
+		if count >= missing {
+			return New(yr, m, day, tp)
 		}
 	}
-	return Date{}, fmt.Errorf("failed to find date for ordinal %d, type %s", ordinal, tp)
+	return Date{}, fmt.Errorf("failed to find date for ordinal %d, type %s", o, tp)
 }
 
 // Helper to return the last int of a slice.
-func lastInt(sl []int) int {
+func lastOrd(sl []Ordinal) Ordinal {
 	if len(sl) == 0 {
 		// Fail fast, fail hard
-		panic("FIXME! lastInt called with zero length int slice")
+		panic("internal error: lastOrd called with zero length int slice")
 	}
 	return sl[len(sl)-1]
+}
+
+/*
+Year returns the best matching year for a given ordinal.
+*/
+func (o Ordinal) Year(tp Type) Year {
+	for yr := progressionTableEnd; yr >= progressionTableStart; yr-- {
+		if YearProgression[Year(yr)][tp] <= o {
+			return Year(yr)
+		}
+	}
+	panic(fmt.Sprintf("internal error: Ordinal.Year failed to find a year for ordinal %d, type %v", o, tp))
 }
--- ordinal_test.go
+++ ordinal_test.go
@@ -2,8 +2,49 @@ package jdcal
 
 import (
 	"testing"
+	"time"
 )
 
 func TestOrdinal(t *testing.T) {
-	// Tested via main/ordinal/ordinal.go and conversions
+	// See also main/demo7/demo7.go
+	for _, ymd := range []YMD{
+		// Near start of epoch
+		{Year: -500, Month: time.January, Day: 1},
+
+		// Around negative leap
+		{Year: -301, Month: time.February, Day: 28},
+		{Year: -301, Month: time.March, Day: 1},
+
+		// Aroound positive leap
+		{Year: 300, Month: time.February, Day: 28},
+		{Year: 300, Month: time.March, Day: 1},
+
+		// Somewhere in the 20th century
+		{Year: 1962, Month: time.August, Day: 19},
+
+		// Near end of epoch
+		{Year: 2100, Month: time.January, Day: 1},
+	} {
+		for _, tp := range []Type{Gregorian, Julian} {
+			dt, err := New(ymd.Year, ymd.Month, ymd.Day, tp)
+			check(t, err)
+			ord, err := dt.Ordinal()
+			check(t, err)
+			back, err := ord.Date(dt.Type)
+			check(t, err)
+
+			eq, err := dt.Equal(back)
+			check(t, err)
+			if !eq {
+				t.Errorf("%v -> %6d -> %v: mismatch", dt, ord, back)
+			}
+
+		}
+	}
+}
+
+func check(t *testing.T, err error) {
+	if err != nil {
+		t.Fatalf("unexpected error %v", err)
+	}
 }
