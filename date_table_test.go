1be9524004bd13c5f64bc36f1f9705b03b13afd1
Karel Kubat <karel@kubat.nl>
2024-01-19 19:02:42 +0100

    limit range

--- date_table_test.go
+++ date_table_test.go
@@ -4,10 +4,23 @@ import "testing"
 
 func TestConversionTable(t *testing.T) {
 	// Test that the table has valid dates.
+
 	for _, e := range ConversionTable {
+		check := func(err error) {
+			if err != nil {
+				t.Fatalf("unexpected error %v", err)
+			}
+		}
 		for _, d := range []Date{e.JDate, e.GDate} {
-			if err := d.Valid(); err != nil {
-				t.Errorf("%+v .Valid() = %q, want nil error", d, err.Error())
+			bf, err := d.Before(First(d.Type))
+			check(err)
+			af, err := d.After(First(d.Type))
+			check(err)
+
+			if !bf && !af {
+				if err := d.Valid(); err != nil {
+					t.Errorf("%+v .Valid() = %q, want nil error", d, err.Error())
+				}
 			}
 		}
 	}
