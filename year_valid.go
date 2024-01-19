88e8628626621a8cc8baa6b59cdb83796c38bc10
Karel Kubat <karel@kubat.nl>
2024-01-19 19:06:11 +0100

    range by algorithm

--- year_valid.go
+++ year_valid.go
@@ -19,11 +19,24 @@ func (y Year) Valid() error {
 	if y == 0 {
 		return errors.New("year 0 does not exist in BC/AD")
 	}
-	if y < First(Julian).Year {
-		return fmt.Errorf("year %v %s", y, beforeConvertibleDate)
+	switch algorithm {
+	case algorithmLookupTable:
+		if y < First(Julian).Year {
+			return fmt.Errorf("year %v %s", y, beforeConvertibleDate)
+		}
+		if y > Last(Gregorian).Year {
+			return fmt.Errorf("year %v %s", y, afterConvertibleDate)
+		}
+		return nil
+	case algorithmProgression:
+		if y < StartProgressionYear {
+			return fmt.Errorf("year %v %s", y, beforeConvertibleDate)
+		}
+		if y > EndProgressionYear {
+			return fmt.Errorf("year %v %s", y, afterConvertibleDate)
+		}
+		return nil
+	default:
+		return errors.New("algorithm mismatch in Year.Valid")
 	}
-	if y > Last(Gregorian).Year {
-		return fmt.Errorf("year %v %s", y, afterConvertibleDate)
-	}
-	return nil
 }
