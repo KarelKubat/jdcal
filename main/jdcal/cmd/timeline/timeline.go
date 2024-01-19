58a4d066cfd7d1d0ee432e83de8190d03b0b2208
Karel Kubat <karel@kubat.nl>
2024-01-19 19:09:23 +0100

    use Type.Other

--- main/jdcal/cmd/timeline/timeline.go
+++ main/jdcal/cmd/timeline/timeline.go
@@ -66,11 +66,7 @@ func runTimeline(cmd *cobra.Command, args []string) {
 		check(err)
 	}
 
-	if dt.Type == jdcal.Julian {
-		fmt.Printf("%11s |   | %11s\n", "Julian", "Gregorian")
-	} else {
-		fmt.Printf("%11s |   | %11s\n", "Gregorian", "Julian")
-	}
+	fmt.Printf("%11s |   | %11s\n", dt.Type, dt.Type.Other())
 	fmt.Println("------------+---+------------")
 
 	var lastDtYear, lastOtYear jdcal.Year
